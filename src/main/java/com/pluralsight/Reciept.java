32c5edd98826021b5fd29c01d2c4b6e3792b2d2d
MikaylaBrabham <mikaylabrabham01@gmail.com>
2026-04-13 13:02:46 -0500

    Added psudocode for receipt

---
+++ src/main/java/com/pluralsight/Reciept.java
@@ -0,0 +1,20 @@
+package com.pluralsight;
+
+public class Reciept {
+    public static void main(String[] args) {
+
+      //  Question 1.
+        // Create string variable for item name
+        // Create double variable for item price
+
+     //  Question 2.
+        // Create a int variable for quantity
+
+
+     // Question 3.
+        // Do operator expression to get price,
+        // Create price variable to print
+        // Create expression statement with variable names to display
+
+    }
+}
